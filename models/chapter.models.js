94966d5f29b54979b21539399d1669931b4768f6
unknown <doniaskima344@gmail.com>
2022-10-15 12:24:30 +0100

    fix the chaptermodel

--- models/chapter.models.js
+++ models/chapter.models.js
@@ -9,6 +9,24 @@ const ChapterSchema = new mongoose.Schema({
         required: true,
     },
     order: { type: Number },
+    readTime: { type: Number, default: 0 },
 }, { timestamps: true });
+
+ChapterSchema.pre("validate", function(next) {
+    this.calculateReadTime();
+    next();
+})
+
+ChapterSchema.methods.calculateReadTime = function() {
+    const wordsPerMinutes = 200;
+    const numberOfWords = this.content.split(/\s/g).length;
+    const minutes = numberOfWords / wordsPerMinutes;
+    const readTime = Math.ceil(minutes);
+    this.readTime = readTime;
+}
+
+// \s means "match whitespace"
+//g is a flag which means "global"
+//==> match all whitespace, not just the first.
 
 module.exports = mongoose.model("Chapter", ChapterSchema, "Chapter");
