74212dd0214591edc6c7d05151c609b52080ebdc
unknown <doniaskima344@gmail.com>
2022-10-21 00:49:35 +0100

    getChapter

--- controllers/chapter.controllers.js
+++ controllers/chapter.controllers.js
@@ -1,6 +1,6 @@
 const chapterModel = require("../models/chapter.models");
 const readModels = require("../models/read.models");
-
+const mongoose = require("mongoose");
 const createChapter = async(req, res) => {
     const newChapter = new chapterModel({
         title: req.body.title,
@@ -31,7 +31,7 @@ const getChapter = async(req, res) => {
         const readExist = await readModels.findOne({
             chapter: chapter._id,
             reader: req.verifiedUser._id,
-        })
+        });
         if (!readExist) {
             const newRead = new readModels({
                 chapter: chapter._id,
@@ -65,58 +65,59 @@ const getChapter = async(req, res) => {
                     as: "comments",
                 },
             },
-            ...(req.verifiedUser ? [{
-                    $lookup: {
-                        from: "Vote",
-                        let: {
-                            chapterId: "$_id",
-                        },
-                        pipeline: [{
-                            $match: {
-                                $expr: {
-                                    $and: {
-                                        $eq: ["$$chapterId", "$chapter"],
-                                        $eq: [
-                                            "$voter",
-                                            mongoose.Types.ObjectId(req.verifiedUser._id),
-                                        ],
+            ...(req.verifiedUser ?
+                [{
+                        $lookup: {
+                            from: "Vote",
+                            let: {
+                                chapterId: "$_id",
+                            },
+                            pipeline: [{
+                                $match: {
+                                    $expr: {
+                                        $and: {
+                                            $eq: ["$$chapterId", "$chapter"],
+                                            $eq: [
+                                                "$voter",
+                                                mongoose.Types.ObjectId(req.verifiedUser._id),
+                                            ],
+                                        },
                                     },
                                 },
-                            },
-                        }, ],
-                        as: "voters",
+                            }, ],
+                            as: "voters",
+                        },
                     },
-                },
-                {
-                    $addFields: {
-                        voters: { $size: "$voters" },
+                    {
+                        $addFields: {
+                            voters: { $size: "$voters" },
+                        },
                     },
-                },
 
-                {
-                    $addFields: {
-                        reads: { $size: "$reads" },
-                        votes: { $size: "$votes" },
-                        comments: { $size: "$comments" },
-                        canVote: {
-                            $switch: {
-                                branches: [
-                                    { case: { $ne: ["$voters", 0] }, then: false },
-                                    { case: { $eq: ["$voters", 0] }, then: true },
-                                ],
+                    {
+                        $addFields: {
+                            reads: { $size: "$reads" },
+                            votes: { $size: "$votes" },
+                            comments: { $size: "$comments" },
+                            canVote: {
+                                $switch: {
+                                    branches: [
+                                        { case: { $ne: ["$voters", 0] }, then: false },
+                                        { case: { $eq: ["$voters", 0] }, then: true },
+                                    ],
+                                },
                             },
                         },
                     },
-                },
-                { $unset: "voters" },
-            ] : []),
+                    { $unset: "voters" },
+                ] :
+                []),
         ]);
         return res.status(200).json(chap[0]);
     } catch (err) {
         return res.status(500).json(err);
     }
-}
-
+};
 
 const deleteChapter = async(req, res) => {
     const chapter = req.chapter;
