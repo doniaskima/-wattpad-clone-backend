3abbe00d4c19f0c2f0d717367113d8a260678487
unknown <doniaskima344@gmail.com>
2022-10-20 15:05:39 +0100

    y raed rabi yahdik

--- controllers/chapter.controllers.js
+++ controllers/chapter.controllers.js
@@ -64,10 +64,53 @@ const getChapter = async(req, res) => {
                     as: "comments",
                 },
             },
-            ...(req.verifiedUser ? [
+            ...(req.verifiedUser ? [{
+                    $lookup: {
+                        from: "Vote",
+                        let: {
+                            chapterId: "$_id",
+                        },
+                        pipeline: [{
+                            $match: {
+                                $expr: {
+                                    $and: {
+                                        $eq: ["$$chapterId", "$chapter"],
+                                        $eq: [
+                                            "$voter",
+                                            mongoose.Types.ObjectId(req.verifiedUser._id),
+                                        ],
+                                    },
+                                },
+                            },
+                        }, ],
+                        as: "voters",
+                    },
+                },
+                {
+                    $addFields: {
+                        voters: { $size: "$voters" },
+                    },
+                },
 
-            ])
-        ])
+                {
+                    $addFields: {
+                        reads: { $size: "$reads" },
+                        votes: { $size: "$votes" },
+                        comments: { $size: "$comments" },
+                        canVote: {
+                            $switch: {
+                                branches: [
+                                    { case: { $ne: ["$voters", 0] }, then: false },
+                                    { case: { $eq: ["$voters", 0] }, then: true },
+                                ],
+                            },
+                        },
+                    },
+                },
+                { $unset: "voters" },
+            ] : []),
+        ]);
+        return res.status(200).json(chap[0]);
     } catch (err) {
         return res.status(500).json(err);
     }
