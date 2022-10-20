7c5e4cd25605a4ad718e4679e82816e95b336589
unknown <doniaskima344@gmail.com>
2022-10-20 14:10:19 +0100

    raed rabi yahdik

--- controllers/chapter.controllers.js
+++ controllers/chapter.controllers.js
@@ -23,7 +23,6 @@ const getStoryChapters = async(req, res) => {
         return res.status(500).json(err);
     }
 };
-
 const getChapter = async(req, res) => {
     const chapter = req.chapter;
     try {
@@ -64,13 +63,53 @@ const getChapter = async(req, res) => {
                     as: "comments",
                 },
             },
-            {
-                $addFields: {
-                    reads: { $size: "$reads" },
-                    votes: { $size: "$votes" },
-                    comments: { $size: "$comments" },
-                },
-            },
+            ...(req.verifiedUser ?
+                [{
+                        $lookup: {
+                            from: "Vote",
+                            let: {
+                                chapterId: "$_id",
+                            },
+                            pipeline: [{
+                                $match: {
+                                    $expr: {
+                                        $and: {
+                                            $eq: ["$$chapterId", "$chapter"],
+                                            $eq: [
+                                                "$voter",
+                                                mongoose.Types.ObjectId(req.verifiedUser._id),
+                                            ],
+                                        },
+                                    },
+                                },
+                            }, ],
+                            as: "voters",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            voters: { $size: "$voters" },
+                        },
+                    },
+
+                    {
+                        $addFields: {
+                            reads: { $size: "$reads" },
+                            votes: { $size: "$votes" },
+                            comments: { $size: "$comments" },
+                            canVote: {
+                                $switch: {
+                                    branches: [
+                                        { case: { $ne: ["$voters", 0] }, then: false },
+                                        { case: { $eq: ["$voters", 0] }, then: true },
+                                    ],
+                                },
+                            },
+                        },
+                    },
+                    { $unset: "voters" },
+                ] :
+                []),
         ]);
         return res.status(200).json(chap[0]);
     } catch (err) {
