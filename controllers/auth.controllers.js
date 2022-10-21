0ce4f83c932cea6aae15af1401716d45d6807d09
unknown <doniaskima344@gmail.com>
2022-10-21 23:02:35 +0100

    forgot and reset password

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -70,6 +70,47 @@ const verifyEmail = async(req, res) => {
     }
 };
 
+const forgotPassword = async(req, res) => {
+    try {
+        const userExist = await userModels.findOne({ email: req.body.email });
+        if (!userExist)
+            return res.status(200).json({ message: "if ur email valid check your inbox to reset password " });
+        const token = jwt.sign({ email: userExist.email },
+            process.env.TOKEN_KEY, {
+                expiresIn: "1h",
+            }
+        );
+        await transporter.sendMail({
+            from: "wattpad@clone-wattpad.com",
+            to: savedUser.email,
+            subject: "reset password",
+            body: `<p>${token}</p>`,
+        })
+        return res.status(200).json({ message: "if ur email valid check your inbox to reset password " });
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+}
+
+const resetPassword = async(req, res) => {
+    try {
+        if (!req.body.token) {
+            return res.status(403).json("no verificarion token provided");
+        }
+        const data = jwt.verify(req.body.token, process.env.TOKEN_KEY);
+        const user = await userModels.findOne({ email: data.email });
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        user.password = hashedPassword;
+        await user.save();
+        return res.status(200).json({ message: "your password was changed" });
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+}
+
 module.exports.login = login;
 module.exports.register = register;
 module.exports.verifyEmail = verifyEmail;
+module.exports.forgotPassword = forgotPassword;
+module.exports.resetPassword = resetPassword;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,16 @@
-const { login, register, verifyEmail } = require("../controllers/auth.controllers");
+const {
+    login,
+    register,
+    verifyEmail,
+    forgotPassword,
+    resetPassword,
+} = require("../controllers/auth.controllers");
 
 const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
 router.post("/verify", verifyEmail)
+router.post("/forgot", forgotPassword);
+router.post("/reset", resetPassword);
 module.exports = router;
